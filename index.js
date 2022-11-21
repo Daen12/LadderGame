55917a5bf150e7616a1ea8f8b174966d62a82a05
Daen12 <sdy971213@gmail.com>
2022-11-21 23:16:28 +0900

    printLadder funtion added

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ if (num < 2 || num > 8) {
     console.log("invalid input");
     process.exit();
 }
+
+function printLadder(num, height) {
+    let count = 0;
+    while (count < height) {
+        count++;
+        let level = new Array(num + 1);
+        level[0] = "";
+        level[num] = ""; //배열의 맨앞과 맨뒤에 "|" 넣을수 있게 빈자리 만들어주기
+        for (i = 1; i < num; i++) {
+            const isStep = Math.random() >= 0.5 ? true : false;
+            const step = isStep ? "-" : " ";
+            level[i] = step;
+        }
+        console.log(level.join("|"));
+    }
+}
+printLadder(num, height);
