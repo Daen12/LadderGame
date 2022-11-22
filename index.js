06ee043af5994f84695cf36fa801ca5ac2057211
Daen12 <sdy971213@gmail.com>
2022-11-22 10:53:05 +0900

    added 2D array version

--- index.js
+++ index.js
@@ -13,11 +13,30 @@ function printLadder(num, height) {
         level[0] = "";
         level[num] = ""; //배열의 맨앞과 맨뒤에 "|" 넣을수 있게 빈자리 만들어주기
         for (i = 1; i < num; i++) {
-            const isStep = Math.random() >= 0.5 ? true : false;
-            const step = isStep ? "-" : " ";
-            level[i] = step;
+            const isStep = Math.random() >= 0.5;
+            level[i] = isStep ? "-" : " ";
         }
         console.log(level.join("|"));
     }
 }
 printLadder(num, height);
+
+// 2차원 배열로 푸는 버전 (문제 다시 읽고 추가..)
+
+function printLadder2(num, height) {
+    let count = 0;
+    const wholeLadder = [];
+    while (count < height) {
+        count++;
+        const level = new Array(num + 1);
+        level[0] = "";
+        level[num] = "";
+        for (let i = 1; i < num; i++) {
+            const isStep = Math.random() >= 0.5;
+            level[i] = isStep ? "-" : " ";
+        }
+        wholeLadder.push(level);
+    }
+    return wholeLadder.map((i) => i.join("|") + "\n").join("");
+}
+console.log(printLadder2(num, height));
