e0850eda22bf6335ad5bc417f6bcf0ec07c8363d
Daen12 <sdy971213@gmail.com>
2022-11-22 10:57:22 +0900

    added 2D array version

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function printLadder(num, height) {
         let level = new Array(num + 1);
         level[0] = "";
         level[num] = ""; //배열의 맨앞과 맨뒤에 "|" 넣을수 있게 빈자리 만들어주기
-        for (i = 1; i < num; i++) {
+        for (let i = 1; i < num; i++) {
             const isStep = Math.random() >= 0.5;
             level[i] = isStep ? "-" : " ";
         }
